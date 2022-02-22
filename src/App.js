2c4b36b0f133b486d124d6b300310ac37b4f16eb
Aravind2679 <aravind102001@gmail.com>
2022-02-22 14:23:53 +0530

    Created Todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './Todo.css';
+import {AiFillEdit,AiFillDelete,AiOutlineCheckCircle} from "react-icons/ai";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Task({ task, index, completeTask, removeTask,editTask }) 
+{
+  console.log(task);
+  const [newName, setNewName] = useState('');
+  const [isEditing, setEditing] = useState(false);
+  const view= (<div
+                  className={task.completed ? "task completed" : "task not-completed" }
+                  style={{ textDecoration: task.completed ? "line-through" : "" }}
+              >
+                  {task.title}
+                 
+
+                  <button style={{ background: "red" }} onClick={() => removeTask(index)}><AiFillDelete/></button>
+                  <button  style={{ background: "green" }} onClick={() => completeTask(index)}><AiOutlineCheckCircle/></button>
+                  <button   onClick={() => setEditing(true)}><AiFillEdit /></button>
+
+
+               </div>
+              );
+
+    function handleChange(e) {
+      setNewName(e.target.value);
+     
+    }
+
+    function handleSubmit(e) {
+      e.preventDefault();
+      editTask(task.id,newName);
+      setNewName("");
+      setEditing(false);
+    }
+  const edit= (
+               <form onSubmit={handleSubmit}>
+               <input
+                    id={task.id}
+                    className="todo-text"
+                    type="text"
+                    value={newName}
+                    onChange={handleChange}
+                    autoFocus
+               />
+               <div className="btn-group">
+              <button
+                type="button"
+                className="btn todo-cancel"
+                onClick={() => setEditing(false)}
+              >
+                Cancel
+                <span className="visually-hidden">renaming {task.title}</span>
+              </button>
+              <button type="submit" className="btn btn__primary todo-edit">
+                Save
+                <span className="visually-hidden">new name for {task.title}</span>
+              </button>
+            </div>
+
+              </form>
   );
+    return (
+               isEditing?edit:view
+    );
 }
+
+function CreateTask({ addTask }) {
+    const [value, setValue] = useState("");
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTask(value);
+        setValue("");
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                placeholder="Add a new task"
+                onChange={e => setValue(e.target.value)}
+            />
+        </form>
+    );
+}
+
+function App()
+ {
+  
+    const [tasksRemaining, setTasksRemaining] = useState(0);
+    const [tasks, setTasks] = useState([
+        {
+            id:Math.floor(Math.random() *1000),
+            title: "Eat",
+            completed: true
+        },
+        {
+            id:Math.floor(Math.random() *1000),
+            title: "Sleep",
+            completed: true
+        },
+        {
+            id:Math.floor(Math.random() *1000),
+            title: "Relax",
+            completed: false
+        }
+    ]);
+
+    useEffect(() => { setTasksRemaining(tasks.filter(task => !task.completed).length) },[tasks]);
+
+
+    const addTask = title => {
+        const newTasks = [...tasks, { id:Math.floor(Math.random() *1000),title, completed: false }];
+        setTasks(newTasks);
+    };
+
+    const completeTask = index => {
+        const newTasks = [...tasks];
+        newTasks[index].completed = true;
+        setTasks(newTasks);
+    };
+
+    const removeTask = index => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    };
+
+    const editTask = (id,newName) => {
+      
+      const editedTaskList = tasks.map(task => {
+        if (id === task.id) {
+          return { ...task, title: newName }
+        }
+        return task;
+      });
+      setTasks(editedTaskList);
+  };
+		    
+    
+
+    return (
+        <div className="todo-container">
+            <div className="header">Pending task(s) ({tasksRemaining})</div>
+            <div className="tasks">
+                {tasks.map((task, index) => (
+                    <Task
+                    task={task}
+                    index={index}
+                    completeTask={completeTask}
+                    removeTask={removeTask}
+                    key={index}
+                    editTask={editTask}
+                    />
+                ))}
+            </div>
+            <div className="create-task" >
+                <CreateTask addTask={addTask} />
+            </div>
+        </div>
+    );
+}
+
+
 
 export default App;
